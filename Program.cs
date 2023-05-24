d43f4d39257a630b51054d8265e9bd18c6a77ebf
Unekwu001 <tusedor360@gmail.com>
2023-05-24 16:40:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Week2;
+ 
+var linkedList = new LinkedListMethods<int>();
+
+linkedList.Add(9);
+linkedList.Add(678);
+linkedList.Add(6);
+
+
+linkedList.PrintLinkedList();
+ 
